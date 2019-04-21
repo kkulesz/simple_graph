a0e4cdad249324c6b6163488145d70cd21538a4f
Konrad <kulesza.konrad@protonmail.com>
2019-04-21 21:20:54 +0200

    added Vertex

---
+++ include/vertex.h
@@ -0,0 +1,15 @@
+#ifndef _VERTEX_H_
+#define _VERTEX_H_
+
+template <class DataType>
+class Vertex{
+public:
+    Vertex(DataType newData, int initIndex): data(newData), index(initIndex){}
+    DataType& getData(){ return data; }
+    int getIndex() const { return index; }
+private:
+    DataType data;
+    int index;
+};
+
+#endif
