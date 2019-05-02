eece320358ae06b169f06a55d0297ba0950bc6a9
Konrad <kulesza.konrad@protonmail.com>
2019-05-02 18:08:34 +0200

    few changes to graph unit tests

--- test/test.cpp
+++ test/test.cpp
@@ -5,39 +5,55 @@
 
 TEST_CASE( "graph", "[graph], [functions]" ){
     Graph<int> int_graph;
-    SECTION(" checking if size after initialization is equal 0 "){
+    SECTION("checking if size after initialization is equal 0"){
         REQUIRE( int_graph.getSize() == 0);
     }
 
-    int_graph.addVertex( 22 );
-    int_graph.addVertex( 5 );
-    int_graph.addVertex( 13 );
-    int_graph.addVertex( 8 );
-    int_graph.addVertex( -5 );
-    int_graph.addVertex( 0 );
+    int data[4] = {58,24,-75,31};
+    int_graph.addVertex( data[0] );
+    int_graph.addVertex( data[1] );
+    int_graph.addVertex( data[2] );
+    int_graph.addVertex( data[3] );
 
-    SECTION(" adding new edge "){
-        REQUIRE( int_graph.addEdge( 0,1 ) );
-    }
-    SECTION(" adding the same edge again "){
-        REQUIRE( int_graph.addEdge( 0,1 ) );
+    SECTION("checking if size gets updated"){
+        REQUIRE( int_graph.getSize() == sizeof(data)/sizeof(data[0]) );
     }
 
-    std::vector<int> vec;
-    int_graph.addEdge( 0,1 );
-    int_graph.addEdge( 1,5 );
+    SECTION("getting data from graph using operator[]"){
+        REQUIRE( int_graph[0] == data[0] );
+    }
+
+    SECTION("changing data using operator[]"){
+        int_graph[0] += 1;
+        REQUIRE( int_graph[0] == data[0]+1 );
+    }
+
+    SECTION("adding new edge "){
+        REQUIRE( int_graph.addEdge( 0,1 ) );
+    }
+    SECTION("adding the same edge again"){
+        REQUIRE( int_graph.addEdge( 0,1 ) );
+    }
+
     SECTION("BFS"){
+    //not sure how to test BFS alghoritm
+    //know it is bad coding below
+        std::vector<int> vec;
+        int_graph.addEdge( 0,1 );
+        int_graph.addEdge( 1,3 );
+        int_graph.addEdge( 1,0 );//should do nothing
+
         REQUIRE( int_graph.BFS(0, vec) );
         REQUIRE( vec.size() == 3 );
         REQUIRE( vec[0] == 0 );
         REQUIRE( vec[1] == 1 );
-        REQUIRE( vec[2] == 5 );
+        REQUIRE( vec[2] == 3 );
     }
 
-    SECTION(" removing edge "){
+    SECTION("removing edge "){
         REQUIRE( int_graph.removeEdge( 0,1 ) );
     }
-    SECTION(" adding edge between non-existing vertexes "){
+    SECTION("adding edge between non-existing vertexes "){
         REQUIRE( !int_graph.addEdge( 100,101 ) );
     }
 }
